3c3d5b3aab52f6ae9cd9c0f0bd7b1c9b7e672369
Victor Chen <yvictorck@gmail.com>
2018-07-19 11:59:05 -0400

    a

--- objtrack.py
+++ objtrack.py
@@ -51,44 +51,42 @@ bbox = (148, 12, 43, 160)
 
 # Initialize tracker with first frame and bounding box
 ok = tracker.init(frame, bbox)
-while video.read():
-    frame = video.read()
-    cv2.imshow("Tracking", frame)
-# while True:
 
-#     # Read a new frame
-#     ok, frame = video.read()
-#     if not ok:
-#         break
+while True:
+
+    # Read a new frame
+    ok, frame = video.read()
+    if not ok:
+        break
      
-#     # Start timer
-#     timer = cv2.getTickCount()
+    # Start timer
+    timer = cv2.getTickCount()
 
-#     # Update tracker
-#     ok, bbox = tracker.update(frame)
+    # Update tracker
+    ok, bbox = tracker.update(frame)
 
-#     # Calculate Frames per second (FPS)
-#     fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer);
+    # Calculate Frames per second (FPS)
+    fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer);
 
-#     # Draw bounding box
-#     if ok:
-#         # Tracking success
-#         p1 = (int(bbox[0]), int(bbox[1]))
-#         p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
-#         cv2.rectangle(frame, p1, p2, (255,0,0), 2, 1)
-#     else :
-#         # Tracking failure
-#         cv2.putText(frame, "Tracking failure detected", (100,80), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,0,255),2)
+    # Draw bounding box
+    if ok:
+        # Tracking success
+        p1 = (int(bbox[0]), int(bbox[1]))
+        p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
+        cv2.rectangle(frame, p1, p2, (255,0,0), 2, 1)
+    else :
+        # Tracking failure
+        cv2.putText(frame, "Tracking failure detected", (100,80), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,0,255),2)
 
-#     # Display tracker type on frame
-#     cv2.putText(frame, tracker_type + " Tracker", (100,20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50),2);
+    # Display tracker type on frame
+    cv2.putText(frame, tracker_type + " Tracker", (100,20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50),2);
  
-#     # Display FPS on frame
-#     cv2.putText(frame, "FPS : " + str(int(fps)), (100,50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50), 2);
+    # Display FPS on frame
+    cv2.putText(frame, "FPS : " + str(int(fps)), (100,50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50), 2);
 
-#     # Display result
-#     cv2.imshow("Tracking", frame)
+    # Display result
+    cv2.imshow("Tracking", frame)
 
-#     # Exit if ESC pressed
-#     k = cv2.waitKey(1) & 0xff
-#     if k == 27 : break
+    # Exit if ESC pressed
+    k = cv2.waitKey(1) & 0xff
+    if k == 27 : break
