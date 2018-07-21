d379a1800958cbc591386fa75dc2194b6b1e5b70
Victor Chen <yvictorck@gmail.com>
2018-07-20 22:46:18 -0400

    a

--- matmult.py
+++ matmult.py
@@ -9,22 +9,22 @@ monitoring_items = ["heart_rate","app_mode","frame_size","timeslice"]
 comm = host_guest_comm.DomU(monitoring_items)
 
 
-it = 1
+
 matsize = 500
 comm.write("app_mode", 4)
 comm.write("frame_size", matsize)
-for i in range(it*6):
+for i in range(50):
 # hb stuff
 	a= np.random.rand(matsize, matsize)
 	b= np.random.rand(matsize, matsize)	
 	tn = time.time()
 	c= np.matmul(b,a.T)
-	print(time.time()-tn)
+	# print(time.time()-tn)
 	time.sleep(0.1)
 	hb.heartbeat_beat()
-	print(hb.get_instant_heartrate())
+	print("instant hr",hb.get_instant_heartrate())
 	if i%window_size_hr==0:
-		comm.write("heart_rate", hb.get_window_heartrate())
+		comm.write("window heart_rate", hb.get_window_heartrate())
 
 
 
