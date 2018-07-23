daf88eaea3cc10cd1061dc6fe32099fc688f65b9
Victor Chen <yvictorck@gmail.com>
2018-07-23 14:03:47 -0400

    a

--- objtrack.py
+++ objtrack.py
@@ -161,9 +161,9 @@ for frame in vidarray:
  
     # show the frame and record if the user presses a key
     cv2.imshow("vid Feed", frame)
-    if text != "sdno car":
-        cv2.imshow("Thresh", thresh)
-        # cv2.imshow("Frame Delta", frameDelta)
+    # if text != "sdno car":
+    #     cv2.imshow("Thresh", thresh)
+    #     # cv2.imshow("Frame Delta", frameDelta)
     # print(cv2.getTickFrequency() / (cv2.getTickCount() - timer))
     fps.update()
 
