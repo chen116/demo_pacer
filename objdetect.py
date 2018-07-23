ba556bae549462dcf23969041ec543c4b67adf09
Victor Chen <yvictorck@gmail.com>
2018-07-23 12:08:18 -0400

    a

--- objdetect.py
+++ objdetect.py
@@ -9,8 +9,11 @@ import cv2
 
 COLORS = np.random.uniform(0, 255, size=(1, 3))
 
+a = [[20,220,84]]
+
 
 # net = cv2.dnn.readNetFromCaffe("MobileNetSSD_deploy.prototxt.txt", "MobileNetSSD_deploy.caffemodel")
 
-
+aa=np.array(a)
 print(COLORS)
+print(aa)
