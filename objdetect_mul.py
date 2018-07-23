95e7f27bafafd1fafc2da0714dec1c0b97a97f0e
Victor Chen <yvictorck@gmail.com>
2018-07-23 17:34:08 -0400

    a

--- objdetect_mul.py
+++ objdetect_mul.py
@@ -247,10 +247,10 @@ for i in range(250):#blank_len+car_len):
 vs.stop()   
 
 rollforward = np.copy(rollback)
-rollforward = np.flip(rollforward, 0)
+rollforward = np.flipud(rollforward)
 carbackword = np.copy(car)
 
-carbackword = np.flip(carbackword,0)
+carbackword = flipud(carbackword)
 vidarray = np.concatenate((blank,blank,car,blank,rollback,car,blank,rollback,car,blank,rollback,car,blank,rollback,car,blank,rollback),axis=0)
 vidarray = np.concatenate((blank,blank,car,blank,rollback,rollforward,blank,carbackword,car,blank,blank),axis=0)
 # vidarray = np.concatenate((blank,blank,car,blank,blank,car,blank,blank,car),axis=0)
