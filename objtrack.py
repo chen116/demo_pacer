46d4088280ceadfe2a44c39abeea858b62d3b345
Victor Chen <yvictorck@gmail.com>
2018-07-23 14:33:22 -0400

    a

--- objtrack.py
+++ objtrack.py
@@ -165,12 +165,12 @@ for frame in vidarray:
     cv2.namedWindow( "Thresh");
     cv2.namedWindow( "Frame Delta" );
     cv2.imshow("vid Feed", frame)
-    if text == "no car":
-        cv2.destroyWindow("Thresh")
-        cv2.destroyWindow("Frame Delta")
-    else:
-        cv2.imshow("Thresh", thresh)
-        cv2.imshow("Frame Delta", frameDelta)
+    # if text == "no car":
+    #     cv2.destroyWindow("Thresh")
+    #     cv2.destroyWindow("Frame Delta")
+    # else:
+    #     cv2.imshow("Thresh", thresh)
+    #     cv2.imshow("Frame Delta", frameDelta)
 
 
     # print(cv2.getTickFrequency() / (cv2.getTickCount() - timer))
@@ -187,15 +187,15 @@ for frame in vidarray:
         f_open=not f_open
 
 
-    # if t_open:
-    #     cv2.imshow("Thresh", thresh)
-    # else:
-    #     if cv2.getWindowProperty("Thresh",cv2.WND_PROP_VISIBLE) >= 1:
-    #         cv2.destroyWindow("Thresh")
-    # if f_open:
-    #     cv2.imshow("Frame Delta", frameDelta)
-    #     if cv2.getWindowProperty("Frame Delta",cv2.WND_PROP_VISIBLE) >= 1:
-    #         cv2.destroyWindow("Frame Delta")
+    if t_open:
+        cv2.imshow("Thresh", thresh)
+    else:
+        if cv2.getWindowProperty("Thresh",cv2.WND_PROP_VISIBLE) >= 1:
+            cv2.destroyWindow("Thresh")
+    if f_open:
+        cv2.imshow("Frame Delta", frameDelta)
+        if cv2.getWindowProperty("Frame Delta",cv2.WND_PROP_VISIBLE) >= 1:
+            cv2.destroyWindow("Frame Delta")
 
     hb.heartbeat_beat()
     print("get_instant_heartrate:",hb.get_instant_heartrate())
