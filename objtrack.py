cc81e679a8b75d700bdacc0bc99d0a9785cb3b11
Victor Chen <yvictorck@gmail.com>
2018-07-23 10:41:48 -0400

    a

--- objtrack.py
+++ objtrack.py
@@ -9,10 +9,10 @@ import time
 import cv2
 
 # # construct the argument parser and parse the arguments
-# ap = argparse.ArgumentParser()
-# ap.add_argument("-v", "--video", help="path to the video file")
-# ap.add_argument("-a", "--min-area", type=int, default=500, help="minimum area size")
-# args = vars(ap.parse_args())
+ap = argparse.ArgumentParser()
+ap.add_argument("-v", "--video", help="path to the video file")
+ap.add_argument("-a", "--min-area", type=int, default=500, help="minimum area size")
+args = vars(ap.parse_args())
  
 # # if the video argument is None, then we are reading from webcam
 # if args.get("video", None) is None:
@@ -25,7 +25,7 @@ import cv2
  
 
 
-vs = FileVideoStream("walkcat.mp4").start()
+vs = FileVideoStream(args["video"]).start()
 time.sleep(2.0)
 
 # initialize the first frame in the video stream
