b961c085998d4d8d572a1452dbaa8aa45dace886
Victor Chen <yvictorck@gmail.com>
2018-07-23 14:27:30 -0400

    a

--- objtrack.py
+++ objtrack.py
@@ -162,16 +162,17 @@ for frame in vidarray:
         (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
  
     # show the frame and record if the user presses a key
-
+    cv2.namedWindow( "Thresh", 1);
+    cv2.namedWindow( "Frame Delta",1 );
     cv2.imshow("vid Feed", frame)
     if text != "sdno car":
-        cv2.namedWindow( "Thresh", 1);
-        cv2.namedWindow( "Frame Delta",1 );
+
         cv2.imshow("Thresh", thresh)
         cv2.imshow("Frame Delta", frameDelta)
     else:
-        cv2.destroyWindow("Thresh")
-        cv2.destroyWindow("Frame Delta")
+        if cv2.getWindowProperty("Thresh",cv2.WND_PROP_VISIBLE) >= 1:
+            cv2.destroyWindow("Thresh")
+            cv2.destroyWindow("Frame Delta")
 
     # print(cv2.getTickFrequency() / (cv2.getTickCount() - timer))
     fps.update()
