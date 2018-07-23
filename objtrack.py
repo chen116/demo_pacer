bde2d587c809b7574f4d5f2001940e76cb1029d0
Victor Chen <yvictorck@gmail.com>
2018-07-23 12:30:02 -0400

    a

--- objtrack.py
+++ objtrack.py
@@ -92,7 +92,7 @@ for frame in vidarray:
         break
  
     # resize the frame, convert it to grayscale, and blur it
-    frame = imutils.resize(frame, width=300)
+    frame = imutils.resize(frame, width=500)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray, (21, 21), 0)
  
