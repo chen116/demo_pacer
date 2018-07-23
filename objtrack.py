60e6769eb01e9cc00c955f376caa4817115fc64f
Victor Chen <yvictorck@gmail.com>
2018-07-23 11:26:23 -0400

    a

--- objtrack.py
+++ objtrack.py
@@ -39,112 +39,114 @@ while vs.more():
 
 
 
-exit()
- 
-# # if the video argument is None, then we are reading from webcam
-# if args.get("video", None) is None:
-#     vs = VideoStream(src=0).start()
-#     time.sleep(2.0)
- 
-# # otherwise, we are reading from a video file
-# else:
-#     vs = cv2.VideoCapture(args["video"])
- 
-vid_len = 150
-vidarray = np.zeros((vid_len,360,640,3),dtype=np.uint8)
-vs= FileVideoStream(args["video"]).start()
-time.sleep(1.0)
-for i in range(vid_len):
-    frame = vs.read()
-    vidarray[i,:,:,:]=frame
-vs.stop()   
 
-
-
-# vs = FileVideoStream(args["video"]).start()
+ 
+# # # if the video argument is None, then we are reading from webcam
+# # if args.get("video", None) is None:
+# #     vs = VideoStream(src=0).start()
+# #     time.sleep(2.0)
+ 
+# # # otherwise, we are reading from a video file
+# # else:
+# #     vs = cv2.VideoCapture(args["video"])
+ 
+# vid_len = 150
+# vidarray = np.zeros((vid_len,360,640,3),dtype=np.uint8)
+# vs= FileVideoStream(args["video"]).start()
 # time.sleep(1.0)
+# for i in range(vid_len):
+#     frame = vs.read()
+#     vidarray[i,:,:,:]=frame
+# vs.stop()   
 
-# initialize the first frame in the video stream
-firstFrame = None
 
-fps = FPS().start()
 
-# loop over the frames of the video
-# while vs.more():
-for frame in vidarray:
+# # vs = FileVideoStream(args["video"]).start()
+# # time.sleep(1.0)
 
-    timer = cv2.getTickCount()
+# # initialize the first frame in the video stream
+# firstFrame = None
 
-    # grab the current frame and initialize the occupied/unoccupied
-    # text
-    # frame = vs.read()
+# fps = FPS().start()
 
-    # frame = frame if args.get("video", None) is None else frame[1]
-    text = "Unoccupied"
- 
-    # if the frame could not be grabbed, then we have reached the end
-    # of the video
-    if frame is None:
-        break
- 
-    # resize the frame, convert it to grayscale, and blur it
-    frame = imutils.resize(frame, width=300)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gray = cv2.GaussianBlur(gray, (21, 21), 0)
- 
-    # if the first frame is None, initialize it
-    if firstFrame is None:
-        firstFrame = gray
-        continue
-    # compute the absolute difference between the current frame and
-    # first frame
-    frameDelta = cv2.absdiff(firstFrame, gray)
-    thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
- 
-    # dilate the thresholded image to fill in holes, then find contours
-    # on thresholded image
-    thresh = cv2.dilate(thresh, None, iterations=2)
-    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
-        cv2.CHAIN_APPROX_SIMPLE)
-    cnts = cnts[0] if imutils.is_cv2() else cnts[1]
- 
-    # loop over the contours
-    for c in cnts:
-        # if the contour is too small, ignore it
-        if cv2.contourArea(c) < args["min_area"]:
-            continue
- 
-        # compute the bounding box for the contour, draw it on the frame,
-        # and update the text
-        (x, y, w, h) = cv2.boundingRect(c)
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        text = "Occupied"
-    # draw the text and timestamp on the frame
-    cv2.putText(frame, "Room Status: {}".format(text), (10, 20),
-        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-    cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"),
-        (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
- 
-    # show the frame and record if the user presses a key
-    cv2.imshow("vid Feed", frame)
-    # cv2.imshow("Thresh", thresh)
-    # cv2.imshow("Frame Delta", frameDelta)
-    cvfps = cv2.getTickFrequency() / (cv2.getTickCount() - timer);
-    print(cvfps)
-    fps.update()
+# # loop over the frames of the video
+# # while vs.more():
+# for frame in vidarray:
 
-    key = cv2.waitKey(1) & 0xFF
+#     timer = cv2.getTickCount()
+
+#     # grab the current frame and initialize the occupied/unoccupied
+#     # text
+#     # frame = vs.read()
+
+#     # frame = frame if args.get("video", None) is None else frame[1]
+#     text = "Unoccupied"
  
-    # if the `q` key is pressed, break from the lop
-    if key == ord("q"):
-        break
+#     # if the frame could not be grabbed, then we have reached the end
+#     # of the video
+#     if frame is None:
+#         break
  
-# cleanup the camera and close any open windows
-fps.stop()
-vs.stop() #if args.get("video", None) is None else vs.release()
-cv2.destroyAllWindows()
-print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
-print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+#     # resize the frame, convert it to grayscale, and blur it
+#     frame = imutils.resize(frame, width=300)
+#     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+#     gray = cv2.GaussianBlur(gray, (21, 21), 0)
+ 
+#     # if the first frame is None, initialize it
+#     if firstFrame is None:
+#         firstFrame = gray
+#         continue
+#     # compute the absolute difference between the current frame and
+#     # first frame
+#     frameDelta = cv2.absdiff(firstFrame, gray)
+#     thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
+ 
+#     # dilate the thresholded image to fill in holes, then find contours
+#     # on thresholded image
+#     thresh = cv2.dilate(thresh, None, iterations=2)
+#     cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+#         cv2.CHAIN_APPROX_SIMPLE)
+#     cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+ 
+#     # loop over the contours
+#     for c in cnts:
+#         # if the contour is too small, ignore it
+#         if cv2.contourArea(c) < args["min_area"]:
+#             continue
+ 
+#         # compute the bounding box for the contour, draw it on the frame,
+#         # and update the text
+#         (x, y, w, h) = cv2.boundingRect(c)
+#         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+#         text = "Occupied"
+#     # draw the text and timestamp on the frame
+#     cv2.putText(frame, "Room Status: {}".format(text), (10, 20),
+#         cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+#     cv2.putText(frame, datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"),
+#         (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+ 
+#     # show the frame and record if the user presses a key
+#     cv2.imshow("vid Feed", frame)
+#     # cv2.imshow("Thresh", thresh)
+#     # cv2.imshow("Frame Delta", frameDelta)
+#     cvfps = cv2.getTickFrequency() / (cv2.getTickCount() - timer);
+#     print(cvfps)
+#     fps.update()
+
+#     key = cv2.waitKey(1) & 0xFF
+ 
+#     # if the `q` key is pressed, break from the lop
+#     if key == ord("q"):
+#         break
+ 
+# # cleanup the camera and close any open windows
+# fps.stop()
+# vs.stop() #if args.get("video", None) is None else vs.release()
+# cv2.destroyAllWindows()
+# print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
+# print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+
+
 # import cv2
 # import sys
  
