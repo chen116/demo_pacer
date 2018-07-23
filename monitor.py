07cabd1b74850e3bd9e4434273925a3dc429eefe
Victor Chen <yvictorck@gmail.com>
2018-07-23 17:42:53 -0400

    a

--- monitor.py
+++ monitor.py
@@ -464,10 +464,14 @@ for t in threads:
 pp = pprint.PrettyPrinter(indent=2)
 print('Final domUs info:')
 shared_data_clean_up = xen_interface.get_global_info()
-default_bw=int(timeslice_us/(len(monitoring_domU)+2.0))
+# default_bw=int(timeslice_us/(len(monitoring_domU)))
 
-for uid in monitoring_domU:
-	xen_interface.sched_rtds(int(uid),timeslice_us,default_bw,[])
+# for uid in monitoring_domU:
+# 	xen_interface.sched_rtds(int(uid),timeslice_us,default_bw,[])
+
+
+
+
 
 # for domuid in shared_data['rtxen']:
 # 	xen_interface.sched_rtds(domuid,timeslice_us,default_bw,[])
