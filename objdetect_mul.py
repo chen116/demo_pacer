4c9ef365cee601bc3a89189eede15cd75534870f
Victor Chen <yvictorck@gmail.com>
2018-07-23 17:08:33 -0400

    a

--- objdetect_mul.py
+++ objdetect_mul.py
@@ -269,6 +269,33 @@ for frame in vidarray:
 	threadLock.release()
 
 
+	stuff={'blob':blob,'cnt':cnt,'n':w1.get()}
+	cnt+=1
+	input_q.put(stuff)
+
+
+    # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # gray = cv2.GaussianBlur(gray, (21, 21), 0)
+ 
+    # # if the first frame is None, initialize it
+    # if firstFrame is None:
+    #     firstFrame = gray
+    #     continue
+    # # compute the absolute difference between the current frame and
+    # # first frame
+    # frameDelta = cv2.absdiff(firstFrame, gray)
+    # thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
+ 
+    # # dilate the thresholded image to fill in holes, then find contours
+    # # on thresholded image
+    # thresh = cv2.dilate(thresh, None, iterations=2)
+    # cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+    #     cv2.CHAIN_APPROX_SIMPLE)
+    # cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+    # for c in cnts:
+    #     # if the contour is too small, ignore it
+    #     if cv2.contourArea(c) > args["min_area"]:
+        	
 
 
 
