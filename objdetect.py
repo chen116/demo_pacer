499d410ea9a2f4f861a938cc1840df97cc4df58b
Victor Chen <yvictorck@gmail.com>
2018-07-23 12:23:19 -0400

    a

--- objdetect.py
+++ objdetect.py
@@ -7,12 +7,13 @@ import imutils
 import time
 import cv2
 
-COLORS = np.random.uniform(0, 255, size=(1, 3))
 # COLORS=np.array([[5,200,250]])
 CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
 	"bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
 	"dog", "horse", "motorbike", "person", "pottedplant", "sheep",
 	"sofa", "train", "tvmonitor"]
+COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
+
 
 net = cv2.dnn.readNetFromCaffe("MobileNetSSD_deploy.prototxt.txt", "MobileNetSSD_deploy.caffemodel")
 
