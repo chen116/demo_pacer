5854f95b9881824e4bea054484864188de5d3289
Victor Chen <yvictorck@gmail.com>
2018-07-23 16:39:50 -0400

    a

--- objdetect_mul.py
+++ objdetect_mul.py
@@ -244,7 +244,7 @@ for i in range(250):#blank_len+car_len):
 
 vs.stop()   
 
-vidarray = np.concatenate((car,blank,rollback,car,blank,rollback),axis=0)
+vidarray = np.concatenate((car,blank,rollback,car,blank,rollback,car,blank,rollback,car,blank,rollback,car,blank,rollback),axis=0)
 
 
 
