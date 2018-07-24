945ae9fa5cfe2b322c631508490e50e02e926da3
Victor Chen <yvictorck@gmail.com>
2018-07-24 12:05:43 -0400

    a

--- dom0_vid_feed.py
+++ dom0_vid_feed.py
@@ -1,10 +1,17 @@
 from pyxs import Client
 import subprocess
+import copy
+# from imutils.video import FileVideoStream
+# from imutils.video import VideoStream
+# from imutils.video import FPS
+# import numpy as np
+# import argparse
+# import imutils
+# import time
+# import cv2
 
 
-
-
-
+# getting domUs ip in centos
 domU_mac = 0
 with Client() as c:
 	domU_mac = (c[b"/local/domain/23/device/vif/0/mac"]).decode()
@@ -17,23 +24,87 @@ domU_ip = p2.communicate()[0].decode().split()[0]
 print(domU_ip)
 
 
-domu_ids=[]
-keys=['vid']
+# ap = argparse.ArgumentParser()
+# ap.add_argument("-v", "--video", help="path to the video file")
+# ap.add_argument("-a", "--min-area", type=int, default=500, help="minimum area size")
+# args = vars(ap.parse_args())
+
+# if "mp4" in args["video"]:
+#     hh=360
+#     ww=480
+
+# else:
+#     hh=144
+#     ww=176
+
+# car_len = 75
+# blank_len = 25
+# rollback_len = 50
+# car = np.zeros((car_len,hh,ww,3),dtype=np.uint8)
+# blank = np.zeros((blank_len,hh,ww,3),dtype=np.uint8)
+# rollback = np.zeros((rollback_len,hh,ww,3),dtype=np.uint8)
+
+
+# vs= FileVideoStream(args["video"]).start()
+# time.sleep(1.0)
+# for i in range(250):#blank_len+car_len):
+#     frame = vs.read()
+#     if i<car_len:
+#         car[i,:,:,:]=frame
+#     elif i < blank_len+car_len:
+#         blank[i-car_len,:,:,:]=frame
+#     elif i >= 200:
+#         rollback[i-200,:,:,:]=frame
+
+
+# vs.stop()   
+
+# rollforward = np.copy(rollback)
+# rollforward = np.flipud(rollforward)
+# carbackword = np.copy(car)
+
+# carbackword = np.flipud(carbackword)
+# vidarray = np.concatenate((blank,blank,car,blank,rollback,car,blank,rollback,car,blank,rollback,car,blank,rollback,car,blank,rollback),axis=0)
+# vidarray = np.concatenate((blank,blank,car,blank,rollback,rollforward,blank,blank,carbackword,blank,blank,car,blank),axis=0)
+
+
+vidarray = [1,2,3]
+
+
 with Client(xen_bus_path="/dev/xen/xenbus") as c:
+	domu_ids=[]
+	keys=['vid_entry']
 	if domu_ids==[]:
 		for x in c.list('/local/domain'.encode()):
 			domu_ids.append(x.decode())
 		domu_ids.pop(0)
+	not_ready_domUs = copy.deepcopy(domu_ids)
 	for domuid in domu_ids:
 		permissions = []
 		permissions.append(('b'+'0').encode())
 		permissions.append(('b'+domuid).encode())
 		for key in keys:
 			tmp_key_path = ('/local/domain'+'/'+domuid+'/'+key).encode()
-			tmp_val = ('xenstore entry init').encode()
+			tmp_val = ('init').encode()
 			c.write(tmp_key_path,tmp_val)
 			c.set_perms(tmp_key_path,permissions)
 			print('created',key,'for dom',domuid)
+
+	print('waiting for domUs getting ready...')
+	while len(not_ready_domUs)>0:
+		ready_domUs = []
+		for domuid in not_ready_domUs:
+			key_path_hash=('/local/domain/'+domu_id+'/vid_entry').encode()
+			if c.read(key_path_hash).decode() == "ready":
+				ready_domUs.append(domu_id)
+		for domuid in ready_domUs:
+			not_ready_domUs.remove(domuid)
+			print("dom",domu_id,"ready")
+
+	print("applcation start...")
+	print(vidarray)
+
+
 
 
 
---
+++ domU_vid_proc.py
@@ -0,0 +1,62 @@
+from pyxs import Client
+import subprocess
+import copy
+from imutils.video import FileVideoStream
+from imutils.video import VideoStream
+from imutils.video import FPS
+import numpy as np
+import argparse
+import imutils
+import time
+import cv2
+
+
+vidarray = [1,2,3]
+
+
+with Client(xen_bus_path="/dev/xen/xenbus") as c:
+	domu_id = c.read("domid".encode())
+	key_path_hash=('/local/domain/'+domu_id.decode()+'/vid_entry').encode()
+	while c.read(key_path_hash).decode() != "init":
+		continue
+	c.write(key_path_hash,('ready').encode())
+	
+
+
+# with Client(xen_bus_path="/dev/xen/xenbus") as c:
+# 	domu_ids=[]
+# 	keys=['vid']
+# 	if domu_ids==[]:
+# 		for x in c.list('/local/domain'.encode()):
+# 			domu_ids.append(x.decode())
+# 		domu_ids.pop(0)
+# 	not_ready_domUs = copy.deepcopy(domu_ids)
+# 	for domuid in domu_ids:
+# 		permissions = []
+# 		permissions.append(('b'+'0').encode())
+# 		permissions.append(('b'+domuid).encode())
+# 		for key in keys:
+# 			tmp_key_path = ('/local/domain'+'/'+domuid+'/'+key).encode()
+# 			tmp_val = ('init').encode()
+# 			c.write(tmp_key_path,tmp_val)
+# 			c.set_perms(tmp_key_path,permissions)
+# 			print('created',key,'for dom',domuid)
+
+# 	print('waiting for domUs getting ready...')
+# 	while len(not_ready_domUs)>0:
+# 		ready_domUs = []
+# 		for domuid in not_ready_domUs:
+# 			key_path_hash=('/local/domain/'+domu_id+'/vid').encode()
+# 			if c.read(key_path_hash).decode() == "ready":
+# 				ready_domUs.append(domu_id)
+# 		for domuid in ready_domUs:
+# 			not_ready_domUs.remove(domuid)
+# 			print("dom",domu_id,"ready")
+
+# 	print("applcation start...")
+# 	print(vidarray)
+
+
+
+
+
