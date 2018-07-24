7acfa237587ca782ec71b6e99c58953c376933d3
Victor Chen <yvictorck@gmail.com>
2018-07-24 13:00:33 -0400

    a

--- dom0_vid_feed.py
+++ dom0_vid_feed.py
@@ -1,74 +1,62 @@
 from pyxs import Client
 import subprocess
 import copy
-# from imutils.video import FileVideoStream
-# from imutils.video import VideoStream
-# from imutils.video import FPS
-# import numpy as np
-# import argparse
-# import imutils
-# import time
-# import cv2
+from imutils.video import FileVideoStream
+from imutils.video import VideoStream
+from imutils.video import FPS
+import numpy as np
+import argparse
+import imutils
+import time
+import cv2
 
 
-# getting domUs ip in centos
-domU_mac = 0
-with Client() as c:
-	domU_mac = (c[b"/local/domain/23/device/vif/0/mac"]).decode()
-# ip n | grep '00:16:3e:6b:67:2e' | cut -d" " -f1
 
-p1 = subprocess.Popen(["ip","n"], stdout=subprocess.PIPE)
-p2 = subprocess.Popen(["grep", domU_mac], stdin=p1.stdout, stdout=subprocess.PIPE)
-p1.stdout.close()  # Allow p1 to receive a SIGPIPE if p2 exits.
-domU_ip = p2.communicate()[0].decode().split()[0]
-print(domU_ip)
+ap = argparse.ArgumentParser()
+ap.add_argument("-v", "--video", help="path to the video file")
+ap.add_argument("-a", "--min-area", type=int, default=500, help="minimum area size")
+args = vars(ap.parse_args())
 
+if "mp4" in args["video"]:
+    hh=360
+    ww=480
 
-# ap = argparse.ArgumentParser()
-# ap.add_argument("-v", "--video", help="path to the video file")
-# ap.add_argument("-a", "--min-area", type=int, default=500, help="minimum area size")
-# args = vars(ap.parse_args())
+else:
+    hh=144
+    ww=176
 
-# if "mp4" in args["video"]:
-#     hh=360
-#     ww=480
-
-# else:
-#     hh=144
-#     ww=176
-
-# car_len = 75
-# blank_len = 25
-# rollback_len = 50
-# car = np.zeros((car_len,hh,ww,3),dtype=np.uint8)
-# blank = np.zeros((blank_len,hh,ww,3),dtype=np.uint8)
-# rollback = np.zeros((rollback_len,hh,ww,3),dtype=np.uint8)
+car_len = 75
+blank_len = 25
+rollback_len = 50
+car = np.zeros((car_len,hh,ww,3),dtype=np.uint8)
+blank = np.zeros((blank_len,hh,ww,3),dtype=np.uint8)
+rollback = np.zeros((rollback_len,hh,ww,3),dtype=np.uint8)
 
 
-# vs= FileVideoStream(args["video"]).start()
-# time.sleep(1.0)
-# for i in range(250):#blank_len+car_len):
-#     frame = vs.read()
-#     if i<car_len:
-#         car[i,:,:,:]=frame
-#     elif i < blank_len+car_len:
-#         blank[i-car_len,:,:,:]=frame
-#     elif i >= 200:
-#         rollback[i-200,:,:,:]=frame
+vs= FileVideoStream(args["video"]).start()
+time.sleep(1.0)
+for i in range(250):#blank_len+car_len):
+    frame = vs.read()
+    if i<car_len:
+        car[i,:,:,:]=frame
+    elif i < blank_len+car_len:
+        blank[i-car_len,:,:,:]=frame
+    elif i >= 200:
+        rollback[i-200,:,:,:]=frame
 
 
-# vs.stop()   
+vs.stop()   
 
-# rollforward = np.copy(rollback)
-# rollforward = np.flipud(rollforward)
-# carbackword = np.copy(car)
+rollforward = np.copy(rollback)
+rollforward = np.flipud(rollforward)
+carbackword = np.copy(car)
 
-# carbackword = np.flipud(carbackword)
-# vidarray = np.concatenate((blank,blank,car,blank,rollback,car,blank,rollback,car,blank,rollback,car,blank,rollback,car,blank,rollback),axis=0)
-# vidarray = np.concatenate((blank,blank,car,blank,rollback,rollforward,blank,blank,carbackword,blank,blank,car,blank),axis=0)
+carbackword = np.flipud(carbackword)
+vidarray = np.concatenate((blank,blank,car,blank,rollback,car,blank,rollback,car,blank,rollback,car,blank,rollback,car,blank,rollback),axis=0)
+vidarray = np.concatenate((blank,blank,car,blank,rollback,rollforward,blank,blank,carbackword,blank,blank,car,blank),axis=0)
 
 
-vidarray = [1,2,3]
+
 
 
 with Client(xen_bus_path="/dev/xen/xenbus") as c:
@@ -106,7 +94,10 @@ with Client(xen_bus_path="/dev/xen/xenbus") as c:
 		key_path_hash=('/local/domain/'+domuid+'/vid_entry').encode()
 		c.write(key_path_hash,domuid.encode())
 
-	print(vidarray)
+	frame_cnt=0
+	for frame in vidarray:
+		cv2.imshow("Frame", frame)
+cv2.destroyAllWindows()
 
 
 
