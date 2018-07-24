90607f6c220cc042b7deeb9a6d2a2a34bcc8774c
Victor Chen <yvictorck@gmail.com>
2018-07-24 18:51:34 -0400

    a

--- monitor.py
+++ monitor.py
@@ -280,9 +280,12 @@ class MonitorThread(threading.Thread):
 				cur_bw=default_bw	
 			cur_bw = 2000/2
 		if self.algo==5:
+			beta=.9
+			free = self.timeslice_us-cur_bw		
 			if(heart_rate<self.min_heart_rate):
 				if cur_bw<self.timeslice_us-minn:
-					cur_bw+=minn
+					free=free*beta
+					cur_bw=self.timeslice_us-free
 			if(heart_rate>self.max_heart_rate):
 				if cur_bw>minn:
 					cur_bw-=minn
