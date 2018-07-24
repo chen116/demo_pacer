4ecd473aaa7889212a86bda2d41de8e056a02ff4
Victor Chen <yvictorck@gmail.com>
2018-07-24 15:26:56 -0400

    a

--- domU_vid_proc.py
+++ domU_vid_proc.py
@@ -54,7 +54,8 @@ carbackword = np.copy(car)
 
 carbackword = np.flipud(carbackword)
 vidarray = np.concatenate((blank,blank,car,blank,rollback,car,blank,rollback,car,blank,rollback,car,blank,rollback,car,blank,rollback),axis=0)
-vidarray = np.concatenate((car,car,blank,blank,car,blank,rollback,rollforward,blank,blank,carbackword,blank,blank,car,blank),axis=0)
+vidarray = np.concatenate((car,carbackword,blank,blank,car,blank,rollback,rollforward,blank,blank,carbackword,blank,blank,car,blank),axis=0)
+vidarray = np.concatenate((car,carbackword,blank,blank,car,carbackword,blank,blank,car,carbackword,blank,blank,car,carbackword,blank,blank),axis=0)
 
 net = cv2.dnn.readNetFromCaffe("MobileNetSSD_deploy.prototxt.txt", "MobileNetSSD_deploy.caffemodel")
 
