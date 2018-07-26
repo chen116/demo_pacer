b896a83d16ed465806246398b09adcedce02064b
Victor Chen <yvictorck@gmail.com>
2018-07-26 10:44:33 -0400

    a

--- monitor_dup3.py
+++ monitor_dup3.py
@@ -66,7 +66,7 @@ class MonitorThread(threading.Thread):
 		self.mid=(min_heart_rate+max_heart_rate)/2
 
 		self.pid = apid.AdapPID(self.mid,1,min_heart_rate,max_heart_rate)
-		self.alloc = res_alloc.ResourceAllocation(args["static_alloc"],timeslice_us,min_heart_rate,max_heart_rate,algo)
+		self.alloc = res_alloc.ResourceAllocation(args["static_alloc"],timeslice_us,min_heart_rate,max_heart_rate,self.algo)
 
 	def run(self):
 		# Acquire lock to synchronize thread
