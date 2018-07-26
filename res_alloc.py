0c72a084bd89b12261554cc32aacd4df3f559ff7
Victor Chen <yvictorck@gmail.com>
2018-07-26 10:56:06 -0400

    a

--- res_alloc.py
+++ res_alloc.py
@@ -12,6 +12,7 @@ class ResourceAllocation:
 		self.pid = apid.AdapPID(self.mid,1,min_heart_rate,max_heart_rate)
 		self.algo = algo
 		self.target_reached_cnt = 0
+		self.minn = timeslice_us * 0.01
 # int allocate_resource(algo, static_alloc, timeslice_us, pid, target_heart_rate,target_reached_cnt)
 	def exec_sharing(self,myinfo,other_info):
 		other_cur_bw = 0
@@ -78,6 +79,7 @@ class ResourceAllocation:
 		return (cur_bw,other_cur_bw)
 
 	def exec_allocation(self,heart_rate,cur_bw):
+
 		if self.algo==0:
 			# static 
 			cur_bw = int(self.timeslice_us*self.static_alloc/100)
@@ -88,14 +90,14 @@ class ResourceAllocation:
 			beta=.9
 			free = self.timeslice_us-cur_bw
 			if(heart_rate<self.mid):
-				if cur_bw<self.timeslice_us-minn:
+				if cur_bw<self.timeslice_us-self.minn:
 					free=free*beta
 					cur_bw=self.timeslice_us-free
 				else:
-					cur_bw=self.timeslice_us-minn
+					cur_bw=self.timeslice_us-self.minn
 			if(heart_rate>self.mid):
-				if cur_bw>minn:
-					free+=alpha*minn
+				if cur_bw>self.minn:
+					free+=alpha*self.minn
 					cur_bw=self.timeslice_us-free
 			cur_bw=int(cur_bw)
 			return(cur_bw)
@@ -107,10 +109,10 @@ class ResourceAllocation:
 			# output+=self.timeslice_us/2
 			if self.pid.start>0:
 				tmp_cur_bw = output+cur_bw #int(output*cur_bw+cur_bw)-int(output*cur_bw+cur_bw)%100
-				if tmp_cur_bw>=self.timeslice_us-minn: #dummy
-					cur_bw=self.timeslice_us-minn
-				elif tmp_cur_bw<=minn:#self.timeslice_us/3:
-					cur_bw=minn#int(self.timeslice_us/3)
+				if tmp_cur_bw>=self.timeslice_us-self.minn: #dummy
+					cur_bw=self.timeslice_us-self.minn
+				elif tmp_cur_bw<=self.minn:#self.timeslice_us/3:
+					cur_bw=self.minn#int(self.timeslice_us/3)
 				else:
 					cur_bw=tmp_cur_bw
 			cur_bw=int(cur_bw)
@@ -123,14 +125,14 @@ class ResourceAllocation:
 			beta=.9
 			free = self.timeslice_us-cur_bw
 			if(heart_rate<self.min_heart_rate):
-				if cur_bw<self.timeslice_us-minn:
+				if cur_bw<self.timeslice_us-self.minn:
 					free=free*beta
 					cur_bw=self.timeslice_us-free
 				else:
-					cur_bw=self.timeslice_us-minn
+					cur_bw=self.timeslice_us-self.minn
 			if(heart_rate>self.max_heart_rate):
-				if cur_bw>minn:
-					free+=alpha*minn
+				if cur_bw>self.minn:
+					free+=alpha*self.minn
 					cur_bw=self.timeslice_us-free
 			cur_bw=int(cur_bw)
 			return(cur_bw)
@@ -140,18 +142,18 @@ class ResourceAllocation:
 			beta=.9
 			free = self.timeslice_us-cur_bw		
 			if(heart_rate<self.min_heart_rate):
-				if cur_bw<self.timeslice_us-minn:
+				if cur_bw<self.timeslice_us-self.minn:
 					free=free*beta
 					cur_bw=self.timeslice_us-free
 			if(heart_rate>self.max_heart_rate):
-				if cur_bw>minn:
-					cur_bw-=minn
+				if cur_bw>self.minn:
+					cur_bw-=self.minn
 			if heart_rate > self.max_heart_rate:
 				self.target_reached_cnt+=1
 				if self.target_reached_cnt==16:
 					self.target_reached_cnt-=8
-					if cur_bw>minn:
-						cur_bw-=minn
+					if cur_bw>self.minn:
+						cur_bw-=self.minn
 			else:
 				self.target_reached_cnt=0
 			return(cur_bw)
