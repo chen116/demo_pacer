a019805e3ade6004739d1668f5e4d471fd3ef034
Victor Chen <yvictorck@gmail.com>
2018-07-27 22:15:05 -0400

    a

--- build/4-Pacer-Dom0-Install.sh
+++ build/4-Pacer-Dom0-Install.sh
@@ -10,6 +10,7 @@ easy_install-3.4 pip
 pip3 install matplotlib 
 pip3 install numpy 
 pip3 install imutils
+pip3 install opencv-python-headless
 wget http://semanchuk.com/philip/sysv_ipc/sysv_ipc-1.0.0.tar.gz && tar -xzf sysv_ipc-1.0.0.tar.gz && cd sysv_ipc-1.0.0 && python3 setup.py install && cd /root/
 git clone https://github.com/selectel/pyxs && cd pyxs && python3 setup.py install && cd /root/ 
 cd /root && git clone https://github.com/chen116/demo_pacer.git && cd demo_pacer
