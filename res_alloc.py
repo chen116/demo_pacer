9ee14a6fe61c7bb09903ac073b8c584c959f88e8
Victor Chen <yvictorck@gmail.com>
2018-07-28 15:38:22 -0400

    a

--- res_alloc.py
+++ res_alloc.py
@@ -99,7 +99,7 @@ class ResourceAllocation:
 		elif self.algo==4:
 			# aimd_range
 			alpha=3.5
-			beta=.9
+			beta=.95
 			free = self.timeslice_us-cur_bw
 			if(heart_rate<self.min_heart_rate):
 				if cur_bw<self.timeslice_us-self.minn:
