29684fadb67303d2a3f98f22fdc4dd35bc53e427
Victor Chen <yvictorck@gmail.com>
2018-07-30 16:09:41 -0400

    a

--- package/Application/matmult.py
+++ package/Application/matmult.py
@@ -1,13 +1,22 @@
 import heartbeat
-import host_guest_comm
+import domU_comm
 import numpy as np
 import time
 
+# create heartbeat
+import heartbeat
 window_size_hr=5
-hb = heartbeat.Heartbeat(1024,window_size_hr,100,"vic.log",10,100)
-monitoring_items = ["heart_rate"]
-comm = host_guest_comm.DomU(monitoring_items)
+sharedmem_id_for_heartbeat = 1024
+buffer_depth = 100
+log_name = "heartbeat.log"
+min_heart_rate = 10 # not used in this demo
+max_heart_rate = 100 # not used in this demo
+hb = heartbeat.Heartbeat(sharedmem_id_for_heartbeat,window_size_hr,buffer_depth,log_name,min_heart_rate,max_heart_rate)
 
+# establish communication with Dom0
+import domU_comm
+monitoring_items = ["heart_rate","frame_size"]
+comm = domU_comm.DomU(monitoring_items)
 
 
 matsize = 500
