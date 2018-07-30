db27787da3d1865c2df6f12743f75e7ccf2d41d3
Victor Chen <yvictorck@gmail.com>
2018-07-30 18:04:25 -0400

    a

--- package/Application/vid_proc.py
+++ package/Application/vid_proc.py
@@ -98,7 +98,7 @@ with Client(xen_bus_path="/dev/xen/xenbus") as c:
 				frame_size = heavy_workload_frame_size
 			else:
 				frame_size = light_workload_frame_size
-			# frame = imutils.resize(frame, width=frame_size)
+			frame = imutils.resize(frame, width=300)
 			(startX, startY, endX, endY)=(0,0,0,0) 
 			blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)),0.007843, (frame_size, frame_size), 127.5)	
 			net.setInput(blob)
