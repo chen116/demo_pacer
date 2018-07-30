41e9c33602a09f9b6bfb4233f0fbf13dd6a50e9d
Victor Chen <yvictorck@gmail.com>
2018-07-30 18:33:29 -0400

    a

--- package/Application/vid_proc.py
+++ package/Application/vid_proc.py
@@ -97,7 +97,7 @@ with Client(xen_bus_path="/dev/xen/xenbus") as c:
 			if detect_car == 1:
 				frame_size = heavy_workload_frame_size
 			else:
-				frame_size = light_workload_frame_size
+				frame_size = heavy_workload_frame_size#light_workload_frame_size
 			frame = imutils.resize(frame, width=frame_size)
 			(startX, startY, endX, endY)=(0,0,0,0) 
 			blob = cv2.dnn.blobFromImage(frame,0.007843, (int(frame_size), int(144*frame_size/176)), 127.5)	
