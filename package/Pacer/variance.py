3f57c2fcde480f5c19dc96662486b6369da409c5
Victor Chen <yvictorck@gmail.com>
2018-07-31 18:28:15 -0400

    a

--- package/Pacer/variance.py
+++ package/Pacer/variance.py
@@ -1,9 +1,9 @@
 import argparse
 ap = argparse.ArgumentParser()
-ap.add_argument("-hr", "--heavy", type=int, default=300, help="heavy-workload-frame-size")
-ap.add_argument("-lr", "--light", type=int, default=150, help="light-workload-frame-size")
-ap.add_argument("-p", "--period",type=int,help="video sequences")
-ap.add_argument("-x", "--util",type=int,help="video sequences")
+ap.add_argument("-hr", "--heavy", type=float, default=300, help="heavy-workload-frame-size")
+ap.add_argument("-lr", "--light", type=float, default=150, help="light-workload-frame-size")
+ap.add_argument("-p", "--period",type=float,default=300,help="video sequences")
+ap.add_argument("-x", "--util",type=float,default=300,help="video sequences")
 args = vars(ap.parse_args())
 
 
@@ -12,6 +12,13 @@ l=args["light"]
 p=args["period"]
 x=args["util"]
 
+
+x=0.5
+p=0.01
+h=0.05
+l=0.024
+
+print("50%")
 print('high')
 print("best case fps:",1/ ((h-2*(p*x))/x+2*(p*x)))
 print("worst case fps:",x/h)
@@ -19,3 +26,22 @@ print("worst case fps:",x/h)
 print('low')
 print("best case fps:",1/ ((l-2*(p*x))/x+2*(p*x)))
 print("worst case fps:",x/l)
+
+
+
+x=0.4
+p=0.01
+h=0.05
+l=0.024
+
+
+print("40%")
+print('high')
+print("best case fps:",1/ ((h-2*(p*x))/x+2*(p*x)))
+print("worst case fps:",x/h)
+
+print('low')
+print("best case fps:",1/ ((l-2*(p*x))/x+2*(p*x)))
+print("worst case fps:",x/l)
+
+
