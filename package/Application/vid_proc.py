59c5aeb3105b0ca26ddb44814b0006f8f286cdb2
Victor Chen <yvictorck@gmail.com>
2018-07-31 11:32:24 -0400

    a

--- package/Application/vid_proc.py
+++ package/Application/vid_proc.py
@@ -149,8 +149,8 @@ with Client(xen_bus_path="/dev/xen/xenbus") as c:
 				for i in np.arange(0, objects_detected.shape[2]):
 					confidence = objects_detected[0, 0, i, 2]
 					if confidence > 0.5:
-						# if motion(frame,prev_frame):
-						# 	print("car moving at frame:", frame_num)
+						if motion(frame,prev_frame):
+							print("car moving at frame:", frame_num)
 						(h, w) = frame.shape[:2]
 						box = objects_detected[0, 0, i, 3:7] * np.array([w, h, w, h])
 						(startX, startY, endX, endY) = box.astype("int")
