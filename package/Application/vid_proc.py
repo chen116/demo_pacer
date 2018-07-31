7f4ce465665b3f57548521123fd1e439b0b82de3
Victor Chen <yvictorck@gmail.com>
2018-07-31 10:56:02 -0400

    a

--- package/Application/vid_proc.py
+++ package/Application/vid_proc.py
@@ -139,7 +139,7 @@ with Client(xen_bus_path="/dev/xen/xenbus") as c:
 			if cnt % 1 ==0:
 				(startX, startY, endX, endY)=(0,0,0,0) 
 
-				blob = cv2.dnn.blobFromImage(cv2.resize(frame, (227, 227)),0.007843, (227, 227), 127.5)				
+				blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)),0.007843, (300, 300), 127.5)				
 				#blob = cv2.dnn.blobFromImage(frame,0.007843, (300, 300), 127.5)				
 				net.setInput(blob)
 				objects_detected = net.forward()
