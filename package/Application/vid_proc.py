e40a896897edf68a34dd65b3743a761bc0200242
Victor Chen <yvictorck@gmail.com>
2018-07-31 10:51:25 -0400

    a

--- package/Application/vid_proc.py
+++ package/Application/vid_proc.py
@@ -13,13 +13,13 @@ import sys
 def motion(frame,prev_frame):
 	if prev_frame is None:
 		return 0
-	prev_frame = cv2.resize(prev_frame, (176*10, 144*10)) #frame_size
+	prev_frame = cv2.resize(prev_frame, (176*7, 144*7)) #frame_size
 	prev_frame = cv2.cvtColor(prev_frame, cv2.COLOR_BGR2GRAY)
 	prev_frame = cv2.GaussianBlur(prev_frame, (21, 21), 0)
 
 
 
-	frame = cv2.resize(frame, (176*10,144*10))#frame_size
+	frame = cv2.resize(frame, (176*7,144*7))#frame_size
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 	gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
@@ -147,7 +147,7 @@ with Client(xen_bus_path="/dev/xen/xenbus") as c:
 					confidence = objects_detected[0, 0, i, 2]
 					if confidence > 0.5:
 						if motion(frame,prev_frame):
-							print("car moving speed")
+							print("car moving at frame:" frame_num)
 						(h, w) = frame.shape[:2]
 						box = objects_detected[0, 0, i, 3:7] * np.array([w, h, w, h])
 						(startX, startY, endX, endY) = box.astype("int")
