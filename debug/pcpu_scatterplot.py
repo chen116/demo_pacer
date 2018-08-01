4940f29cbe88b0a356daba55377cc6fa2c92bb0f
Victor Chen <yvictorck@gmail.com>
2018-08-01 19:18:11 -0400

    a

--- debug/pcpu_scatterplot.py
+++ debug/pcpu_scatterplot.py
@@ -8,9 +8,18 @@ ap.add_argument("-f", "--file", default="data.txt", help="file")
 args = vars(ap.parse_args())
 
 
+s = pd.Series()
+
 pcpu = []
 data = open(args["file"],"r").read()
 for eachLine in data:
     if len(eachLine)>1:
-        line = eachLine.split()
+    	line = eachLine.split()
+    	if len(line) == 3 :
+        # get line with structure 0v1 5.2343243 1
+        	vmid = int(line[0].split('v')[0])
+        	vcpu = int(line[0].split('v')[1])
+        	time = float(line[1])
+        	pcpu = int(line[2])
+        
         VMs_id.append(float(line[1]))
