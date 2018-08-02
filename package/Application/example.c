cf2462748196090c58bf4836ac6aacaa1033f486
Victor Chen <yvictorck@gmail.com>
2018-08-02 15:35:24 -0400

    a

--- package/Application/example.c
+++ package/Application/example.c
@@ -3,7 +3,25 @@
 #include <stdlib.h>
 #include <xenstore.h> // Prior to Xen 4.2.0 use xs.h
 
+#include <heartbeats/heartbeat.h>
+#include <string.h>
 
+
+
+
+
+
+heartbeat_t* heart;
+
+
+
+
+
+static const int64_t vic_win_size = 10;
+static const int64_t vic_buf_depth = 1000;
+static const char* vic_log_file ="vic.log";
+static const int64_t vic_min_target = 100;
+static const int64_t vic_max_target = 1000;
 
 //git pull && gcc -Wall example.c -o out  -lxenstore && ./out
 
