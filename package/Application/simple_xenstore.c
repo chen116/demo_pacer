6d325cb9b8ee80c5b7bf44ba59c6ed362ee87703
Victor Chen <yvictorck@gmail.com>
2018-08-02 15:52:21 -0400

    a

---
+++ package/Application/simple_xenstore.c
@@ -0,0 +1,30 @@
+// gcc -Wall simple_xenstore.c -o simple -lxenstore 
+
+#include <stdlib.h>
+#include <xenstore.h> 
+
+
+int main( int argc, const char** argv )
+{
+    char *path;
+    int er;
+    struct xs_handle *xs;
+    xs_transaction_t th;
+
+
+    // create and establish connection to xenstore with a path
+    xs = xs_daemon_open();
+    path = xs_get_domain_path(xs, 4); // domU id , I put 4 for now
+    path = (char*)realloc(path, strlen(path) + strlen("/heart_rate") + 1);
+    if ( path == NULL ) printf("not good\n");
+    strcat(path, "/heart_rate");
+    printf("%s\n",path);
+
+	//write to xenstore
+    th = xs_transaction_start(xs);
+    er = xs_write(xs, th, path, "11.0", strlen("11.0"));
+    xs_transaction_end(xs, th, false);
+
+	xs_daemon_close(xs);
+	free(path);
+}
