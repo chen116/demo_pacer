1e15b08349fe3342340f1568132da4428d00bbb9
Victor Chen <yvictorck@gmail.com>
2018-08-02 15:42:22 -0400

    a

--- package/Application/example.cpp
+++ package/Application/example.cpp
@@ -1,20 +1,140 @@
 
 
-
-//git pull && gcc example.cpp -lhb-shared -lhrm-shared -lxenstore -std=c++11  && ./a.out
 #include <stdlib.h>
 #include <xenstore.h> // Prior to Xen 4.2.0 use xs.h
 
+#include <heartbeats/heartbeat.h>
+
+
+
+
+
+
+
+heartbeat_t* heart;
+
+
+
+
+
+static const int64_t vic_win_size = 10;
+static const int64_t vic_buf_depth = 1000;
+static const char* vic_log_file ="vic.log";
+static const int64_t vic_min_target = 100;
+static const int64_t vic_max_target = 1000;
+
+//git pull && gcc -Wall example.c -o out  -lxenstore -lhb-shared && ./out
+void matmult(int **,int **,int **,int);
+
+void matmult(int ** ptr1,int ** ptr2, int ** ptr3,int N){
+
+    int i, j, k;
+
+
+
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < N; j++) {
+            ptr3[i][j] = 0;
+            for (k = 0; k < N; k++)
+                ptr3[i][j] = ptr3[i][j] + ptr1[i][k] * ptr2[k][j];
+        }
+    }
+
+    return;
+}
 
 
 int main( int argc, const char** argv )
 {
-
+char *path;
+int er;
 struct xs_handle *xs;
 xs_transaction_t th;
 xs = xs_daemon_open();
+path = xs_get_domain_path(xs, 4); 
+path = (char*)realloc(path, strlen(path) + strlen("/heart_rate") + 1);
+if ( path == NULL ) printf("not good\n");
+strcat(path, "/heart_rate");
+printf("%s\n",path);
+
+ heart = heartbeat_init(vic_win_size, vic_buf_depth, vic_log_file, vic_min_target, vic_max_target);
+
+
+    int **ptr1, **ptr2, **ptr3;
+    int  col1, row2, col2;
+    int N=130;
+    int j,i;
+    ptr1 = (int **) malloc (sizeof (int *) * N);
+    ptr2 = (int **) malloc (sizeof (int *) * N);
+    ptr3 = (int **) malloc (sizeof (int *) * N);
+    for (i = 0; i < N; i++)
+        ptr1[i] = (int *) malloc (sizeof (int) * N);
+    for (i = 0; i < N; i++)
+        ptr2[i] = (int *) malloc (sizeof (int) * N);
+    for (i = 0; i < N; i++)
+        ptr3[i] = (int *) malloc (sizeof (int) * N);
+
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < N; j++) {
+            ptr1[i][j] = rand ()%10;
+        }
+    }
+
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < N; j++) {
+            ptr2[i][j] = rand ()%10;
+        }
+    }
+    for (i = 0; i < 5; ++i)
+    {
+
+
+        matmult(ptr1,ptr2,ptr3,N);
+        matmult(ptr1,ptr2,ptr3,N);
+
+        heartbeat(heart, 1);
+        // char hr_str[10];
+        // gcvt(hb_get_instant_rate(heart) , 6, hr_str);
+        // printf("%s %d \n", hr_str, strlen(hr_str));
+        th = xs_transaction_start(xs);
+        er = xs_write(xs, th, path, "11.0", strlen("11.0"));
+        xs_transaction_end(xs, th, false);
+        printf("heartbeat: instant rate: %f\n",hb_get_instant_rate(heart) );
+    }
+    
+
+
+    /** Printing the contents of third matrix. */
+
+    printf ("\n");
+
+th = xs_transaction_start(xs);
+er = xs_write(xs, th, path, "done", strlen("done"));
+xs_transaction_end(xs, th, false);
+
+
+    printf ("\n");
+    heartbeat_finish(heart);
     return (0);
+
 }
+
+
+// //git pull && gcc example.cpp -lhb-shared -lhrm-shared -lxenstore -std=c++11  && ./a.out
+// #include <stdlib.h>
+// #include <xenstore.h> // Prior to Xen 4.2.0 use xs.h
+
+// //gcc -Wall example.c -o example.out  -lxenstore
+
+// int main( int argc, const char** argv )
+// {
+
+// struct xs_handle *xs;
+// xs_transaction_t th;
+// xs = xs_daemon_open();
+// printf("hi\n");
+//     return (0);
+// }
 
 
 // //git pull && gcc example.cpp -lhb-shared -lhrm-shared -lxenstore -std=c++11  && ./a.out
@@ -43,24 +163,7 @@ xs = xs_daemon_open();
 // static const int64_t vic_min_target = 100;
 // static const int64_t vic_max_target = 1000;
 
-// void matmult(int **,int **,int **,int);
 
-// void matmult(int ** ptr1,int ** ptr2, int ** ptr3,int N){
-
-//     int i, j, k;
-
-
-
-//     for (i = 0; i < N; i++) {
-//         for (j = 0; j < N; j++) {
-//             ptr3[i][j] = 0;
-//             for (k = 0; k < N; k++)
-//                 ptr3[i][j] = ptr3[i][j] + ptr1[i][k] * ptr2[k][j];
-//         }
-//     }
-
-//     return;
-// }
 
 
 // int main ()
@@ -78,16 +181,16 @@ xs = xs_daemon_open();
 // char * buf;
 // unsigned int len;
 // /* Get a connection to the daemon */
-// /*Attempts to make a connection over the socket interface,
+// Attempts to make a connection over the socket interface,
 //  * and if it fails, then over the  xenbus interface.
 //  * Mode 0 specifies read-write access, XS_OPEN_READONLY for
-//  * read-only access.*/
+//  * read-only access.
 // xs = xs_daemon_open();
 // if ( xs == NULL ) printf("not good\n");
 // /* Get the local domain path */
 
 
-// path = xs_get_domain_path(xs, 5); // replace "domid" with a valid domain ID (or one which will become valid)
+// path = xs_get_domain_path(xs, 4); // replace "domid" with a valid domain ID (or one which will become valid)
 // if ( path == NULL ) printf("not good\n");
 // /* Make space for our node on the path */
 // path = (char*)realloc(path, strlen(path) + strlen("/heart_rate") + 1);
