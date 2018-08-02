3e361e0338ef5f16db1e5a604e35d32532361fbb
Victor Chen <yvictorck@gmail.com>
2018-08-02 14:44:21 -0400

    a

--- package/Application/example.cpp
+++ package/Application/example.cpp
@@ -2,8 +2,8 @@
 
 
 //git pull && gcc example.cpp -lhb-shared -lhrm-shared -lxenstore -std=c++11  && ./a.out
-#include <stdlib.h>
 #include <xenstore.h> // Prior to Xen 4.2.0 use xs.h
+#include <stdlib.h>
 
 #include <stdio.h>
 #include <time.h>
