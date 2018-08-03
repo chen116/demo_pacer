8d466c9d4381a22a1febd13a4b6fa30f6330159f
Victor Chen <yvictorck@gmail.com>
2018-08-03 16:07:44 -0400

    a

--- package/Application/cpp/yolo/vid_proc.cpp
+++ package/Application/cpp/yolo/vid_proc.cpp
@@ -63,7 +63,7 @@ int main(int argc, char** argv)
 {
 
 // system("python3 getDomUid.py > id.txt"); 
-system("python3 -c \'from pyxs import Client;c=Client(xen_bus_path=\"/dev/xen/xenbus\");c.connect();print((c.read(\"domid\".encode())).decode());c.close()\'
+system("python3 -c \'from pyxs import Client;c=Client(xen_bus_path="/dev/xen/xenbus");c.connect();print((c.read("domid".encode())).decode());c.close()\'
  > id.txt"); 
 
 
