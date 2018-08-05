f1300ad0a093bfe10abeba8e03d0707a9ffe8bfe
Victor Chen <yvictorck@gmail.com>
2018-08-05 16:39:23 -0400

    a

--- package/Application/cpp/yolo/xen_comm.c
+++ package/Application/cpp/yolo/xen_comm.c
@@ -12,8 +12,25 @@ static const int64_t vic_max_target = 1000;
 
 int main( )
 {
+    char *path;
+	int er;
+	char * buf;
+	// unsigned int *len =  (unsigned int*) malloc(sizeof(unsigned int));
+	// int len = 42;
 
+	unsigned int len;
+    struct xs_handle *xs;
+    xs_transaction_t th;
+    xs = xs_daemon_open();
 
+    path = xs_get_domain_path(xs, 10); 
+    path = (char*)realloc(path, strlen(path) + strlen("/frame_number_entry") + 1);
+    strcat(path, "/frame_number_entry");
+    printf("%s\n",path);
+	th = xs_transaction_start(xs);
+	buf = xs_read(xs, th, path, &len);
+	xs_transaction_end(xs, th, false);
+	printf("%s\n",buf );
 
 	return 0;
 }
