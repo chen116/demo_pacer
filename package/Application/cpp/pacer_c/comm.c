fceac15b108f4730a2a338b58f35efb62065612f
Victor Chen <yvictorck@gmail.com>
2018-08-06 16:43:57 -0400

    a

--- package/Application/cpp/pacer_c/comm.c
+++ package/Application/cpp/pacer_c/comm.c
@@ -1,7 +1,6 @@
 
 #include "comm.h"
 #include <stdio.h>
-#include <xenstore.h>
 #include <heartbeats/heartbeat.h>
 #include <string.h>
 heartbeat_t* heart;
@@ -39,27 +38,27 @@ int getDomid()
    return domid;
 }
 
-// void xenstore_write(struct xs_handle *xs, xs_transaction_t th, const char *path, const void *data)
-// {
+void xenstore_write(struct xs_handle *xs, xs_transaction_t th, const char *path, const void *data)
+{
 
-// 	    // heartbeat(heart, 1);
-//         // printf("heartbeat: instant rate: %f\n",hb_get_instant_rate(heart) );
-//         // char hr_str[10];
-//         // gcvt(hb_get_instant_rate(heart) , 8, hr_str);
-//         // th = xs_transaction_start(xs);
-//         // er = xs_write(xs, th, path, hr_str, strlen(hr_str));
-//         // xs_transaction_end(xs, th, false);
-// 		int er;
-//         th = xs_transaction_start(xs);
-//         er = xs_write(xs, th, path, data, strlen(data));
-//         xs_transaction_end(xs, th, false);
-//         return;
-// }
-// char* xenstore_read(struct xs_handle* xs ,xs_transaction_t th, const char* path , unsigned int *len )
-// {
-// 	char * buf;
-// 	th = xs_transaction_start(xs);
-// 	buf = xs_read(xs, th, path, len);
-// 	xs_transaction_end(xs, th, false);
-// 	return buf;
-// }
+	    // heartbeat(heart, 1);
+        // printf("heartbeat: instant rate: %f\n",hb_get_instant_rate(heart) );
+        // char hr_str[10];
+        // gcvt(hb_get_instant_rate(heart) , 8, hr_str);
+        // th = xs_transaction_start(xs);
+        // er = xs_write(xs, th, path, hr_str, strlen(hr_str));
+        // xs_transaction_end(xs, th, false);
+		int er;
+        th = xs_transaction_start(xs);
+        er = xs_write(xs, th, path, data, strlen(data));
+        xs_transaction_end(xs, th, false);
+        return;
+}
+char* xenstore_read(struct xs_handle* xs ,xs_transaction_t th, const char* path , unsigned int *len )
+{
+	char * buf;
+	th = xs_transaction_start(xs);
+	buf = xs_read(xs, th, path, len);
+	xs_transaction_end(xs, th, false);
+	return buf;
+}
