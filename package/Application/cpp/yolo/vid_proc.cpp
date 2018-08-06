3e379799d7eb0e2d402e63e355d2883e696b2dd5
Victor Chen <yvictorck@gmail.com>
2018-08-06 13:57:18 -0400

    a

--- package/Application/cpp/yolo/vid_proc.cpp
+++ package/Application/cpp/yolo/vid_proc.cpp
@@ -76,10 +76,10 @@ extern "C" int xenstore_write(struct xs_handle *h, xs_transaction_t t, const cha
 #include <array>
 
 int exec(const char* cmd) {
-    std::array<char, 4> buffer;
-    std::string result;
-    std::shared_ptr<FILE> pipe(popen(cmd, "r"), pclose);
-    if (!pipe) throw std::runtime_error("popen() failed!");
+    array<char, 4> buffer;
+    string result;
+    shared_ptr<FILE> pipe(popen(cmd, "r"), pclose);
+    if (!pipe) throw runtime_error("popen() failed!");
     while (!feof(pipe.get())) {
         if (fgets(buffer.data(), 4, pipe.get()) != nullptr)
             result += buffer.data();
