e7d97350f0f9fded206d16df7db7c0bd9c937cb4
Victor Chen <yvictorck@gmail.com>
2018-08-06 17:59:32 -0400

    a

--- package/Application/cpp/pacer_c/comm.c
+++ package/Application/cpp/pacer_c/comm.c
@@ -49,7 +49,7 @@ int getDomid()
         stream =popen("python3 -c \'from pyxs import Client;c=Client(xen_bus_path=\"/dev/xen/xenbus\");c.connect();print((c.read(\"domid\".encode())).decode());c.close()\'", "r");
        if (stream) {
         while (!feof(stream))
-        if (fgets(buffer, max_buffer, stream) != NULL) printf("%s\n",buffer );;
+        if (fgets(buffer, max_buffer, stream) != NULL) printf("%s\n",buffer );
         pclose(stream);
         }
 
