75eae7930e6e8cabb70cadc3a94145d5eaa860c6
Victor Chen <yvictorck@gmail.com>
2018-08-06 17:28:54 -0400

    a

--- package/Application/cpp/pacer_c/comm.h
+++ package/Application/cpp/pacer_c/comm.h
@@ -1,6 +1,12 @@
 
 #include <stdio.h>
+#ifdef __cplusplus
+extern "C" {
+#endif
 #include <xenstore.h>
+#ifdef __cplusplus
+}
+#endif
 
 
 char * xenstore_read(struct xs_handle*  ,xs_transaction_t , const char*  , unsigned int * );
