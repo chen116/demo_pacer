36ae270fa42c5643a32e35295ac93a942bbea8b7
Victor Chen <yvictorck@gmail.com>
2018-08-06 17:29:34 -0400

    a

--- package/Application/cpp/pacer_c/comm.h
+++ package/Application/cpp/pacer_c/comm.h
@@ -7,8 +7,9 @@ extern "C" {
 #ifdef __cplusplus
 }
 #endif
+extern "C" char * xenstore_read(struct xs_handle*  ,xs_transaction_t , const char*  , unsigned int * );
+extern "C" int xenstore_write(struct xs_handle *, xs_transaction_t , const char *, const void *);
 
-
-char * xenstore_read(struct xs_handle*  ,xs_transaction_t , const char*  , unsigned int * );
-void xenstore_write(struct xs_handle *, xs_transaction_t , const char *, const void *);
+// char * xenstore_read(struct xs_handle*  ,xs_transaction_t , const char*  , unsigned int * );
+// void xenstore_write(struct xs_handle *, xs_transaction_t , const char *, const void *);
 int getDomid();
