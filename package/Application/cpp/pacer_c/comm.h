b0828d2b3d8ddecfb5ee20db12b17b955125e9e3
Victor Chen <yvictorck@gmail.com>
2018-08-06 17:11:20 -0400

    a

--- package/Application/cpp/pacer_c/comm.h
+++ package/Application/cpp/pacer_c/comm.h
@@ -4,5 +4,5 @@
 
 
 char * xenstore_read(struct xs_handle*  ,xs_transaction_t , const char*  , unsigned int * );
-int xenstore_write(struct xs_handle *, xs_transaction_t , const char *, const void *);
+void xenstore_write(struct xs_handle *, xs_transaction_t , const char *, const void *);
 int getDomid();
