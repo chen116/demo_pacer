e80602c2898259a2d072c7abbd5b3e01f9e90975
Victor Chen <yvictorck@gmail.com>
2018-08-06 17:50:47 -0400

    a

--- package/Application/cpp/pacer_c/comm.c
+++ package/Application/cpp/pacer_c/comm.c
@@ -34,7 +34,7 @@ int getDomid()
    FILE *in=NULL;
    char temp[4];
    printf("hiii\n");
-   in=popen("python3 -c 'from pyxs import Client;c=Client(xen_bus_path=\"/dev/xen/xenbus\");c.connect();print((c.read(\"domid\".encode())).decode());c.close()'", "r");
+   in=popen("python3 -c \'from pyxs import Client;c=Client(xen_bus_path=\"/dev/xen/xenbus\");c.connect();print((c.read(\"domid\".encode())).decode());c.close()\'", "r");
    fgets(domid, 4, in);
    domid = atoi(domid) - 1;
    return domid;
