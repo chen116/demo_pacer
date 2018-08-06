24208846c6d6ea176fc60b545e5a0baee4ee253e
Victor Chen <yvictorck@gmail.com>
2018-08-06 18:32:16 -0400

    a

--- package/Application/cpp/pacer_c/comm.h
+++ package/Application/cpp/pacer_c/comm.h
@@ -7,7 +7,15 @@ extern "C" {
 #ifdef __cplusplus
 }
 #endif
+#include <heartbeats/heartbeat.h>
 
+struct pacer {
+        int domid;
+        struct xs_handle* xs;
+        struct xs_transaction_t th;
+        char * base_path;
+        heartbeat_t * heart;
+};
 // extern "C" char * xenstore_read(struct xs_handle*  ,xs_transaction_t , const char*  , unsigned int * );
 // extern "C" int xenstore_write(struct xs_handle *, xs_transaction_t , const char *, const void *);
 
