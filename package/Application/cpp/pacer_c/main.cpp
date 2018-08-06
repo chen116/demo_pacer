4465ea5e1150bec65e0ae8d06c6aaf8e539a4b76
Victor Chen <yvictorck@gmail.com>
2018-08-06 17:38:19 -0400

    a

--- package/Application/cpp/pacer_c/main.cpp
+++ package/Application/cpp/pacer_c/main.cpp
@@ -4,6 +4,7 @@ extern "C" {
 #include "comm.h"
 #ifdef __cplusplus
 }
+#endif
 int main(int argc, char** argv)
 {
 	int domid=getDomid();//exec(R"(python3 -c 'from pyxs import Client;c=Client(xen_bus_path="/dev/xen/xenbus");c.connect();print((c.read("domid".encode())).decode());c.close()')"); 
