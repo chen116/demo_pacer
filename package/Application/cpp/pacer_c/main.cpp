e31bc80cc3df8b2c62e1c8b059bae5f19eb7d289
Victor Chen <yvictorck@gmail.com>
2018-08-06 23:14:47 -0400

    a

--- package/Application/cpp/pacer_c/main.cpp
+++ package/Application/cpp/pacer_c/main.cpp
@@ -18,8 +18,20 @@ pacer mypacer;
 // mypacer.base_path = xs_get_domain_path(xs, domid);
 
 
+
+
+
 int main(int argc, char** argv)
 {
+
+	// init_pacer() 
+	// add_entry()
+	// heartbeat()
+	// send()
+	// finish()
+
+
+
 	int domid=getDomid();//exec(R"(python3 -c 'from pyxs import Client;c=Client(xen_bus_path="/dev/xen/xenbus");c.connect();print((c.read("domid".encode())).decode());c.close()')"); 
     printf("domid %d\n",domid);
     mypacer.domid = domid;
