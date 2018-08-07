baccbb06a86339416eeb753f364555221abc8bfb
Victor Chen <yvictorck@gmail.com>
2018-08-07 09:42:05 -0400

    a

--- package/Application/cpp/pacer_cpp/pacer.cpp
+++ package/Application/cpp/pacer_cpp/pacer.cpp
@@ -32,7 +32,11 @@ int exec() {
 }
 
 #include "pacer.h"
-
+static const int64_t vic_win_size = 10;
+static const int64_t vic_buf_depth = 1000;
+static const char* vic_log_file ="vic.log";
+static const int64_t vic_min_target = 100;
+static const int64_t vic_max_target = 1000;
 Pacer::Pacer()
 {
  	domid = xenstore_getDomid();
