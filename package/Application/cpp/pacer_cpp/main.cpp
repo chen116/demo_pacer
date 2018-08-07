92ca0728d51db8a7cbfb22acae6d8de26dc4ed2a
Victor Chen <yvictorck@gmail.com>
2018-08-07 08:50:00 -0400

    a

--- package/Application/cpp/pacer_cpp/main.cpp
+++ package/Application/cpp/pacer_cpp/main.cpp
@@ -1,17 +1,11 @@
-#ifdef __cplusplus
-extern "C" {
-#endif
-#include "comm.h"
-#ifdef __cplusplus
-}
-#endif
+#include "pacer.h"
 
 static const int64_t vic_win_size = 10;
 static const int64_t vic_buf_depth = 1000;
 static const char* vic_log_file ="vic.log";
 static const int64_t vic_min_target = 100;
 static const int64_t vic_max_target = 1000;
-pacer mypacer;
+
 // mypacer.heart = heartbeat_init(vic_win_size, vic_buf_depth, vic_log_file, vic_min_target, vic_max_target);
 // mypacer.domid = getDomid();  
 // mypacer.xs = xs_daemon_open();
@@ -30,6 +24,8 @@ int main(int argc, char** argv)
 	// send()
 	// finish()
 
+	Pacer mypacer(2,3);
+	printf("%d\n",mypacer.getSum() );
 
 
 	int domid=getDomid();//exec(R"(python3 -c 'from pyxs import Client;c=Client(xen_bus_path="/dev/xen/xenbus");c.connect();print((c.read("domid".encode())).decode());c.close()')"); 
