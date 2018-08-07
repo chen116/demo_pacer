4270451624651903f75439ff9b0ea1e9a78983e7
Victor Chen <yvictorck@gmail.com>
2018-08-07 10:13:07 -0400

    a

--- package/Application/cpp/pacer_cpp/pacer.cpp
+++ package/Application/cpp/pacer_cpp/pacer.cpp
@@ -54,7 +54,7 @@ char* Pacer::read(char * item)
 	printf("mee  %s\n",item);
 	for (map<char *,char *>::iterator it=paths.begin(); it!=paths.end(); ++it)
 	{
-    	if (it->first==item) cout << it->second <<endl;
+    	if (it->first=="heart_rate") cout << it->second <<endl;
 	}
 
 	printf("%s\n", paths.find(item)->second );
