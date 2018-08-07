59c864e17a62c43115259ebe626d6996d096cdb3
Victor Chen <yvictorck@gmail.com>
2018-08-07 09:11:30 -0400

    a

--- package/Application/cpp/pacer_cpp/pacer.h
+++ package/Application/cpp/pacer_cpp/pacer.h
@@ -1,7 +1,16 @@
+
+#include <stdio.h>
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include <xenstore.h>
+#ifdef __cplusplus
+}
+#endif
+#include <heartbeats/heartbeat.h>
 class Pacer
 {
 private:
-  int domid;
     int domid;
     struct xs_handle* xs;
     xs_transaction_t th;
