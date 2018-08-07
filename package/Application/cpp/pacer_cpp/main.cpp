92c39f2d13816739229d87d0e7704f27012705d5
Victor Chen <yvictorck@gmail.com>
2018-08-07 08:50:54 -0400

    a

--- package/Application/cpp/pacer_cpp/main.cpp
+++ package/Application/cpp/pacer_cpp/main.cpp
@@ -1,10 +1,5 @@
 #include "pacer.h"
 
-static const int64_t vic_win_size = 10;
-static const int64_t vic_buf_depth = 1000;
-static const char* vic_log_file ="vic.log";
-static const int64_t vic_min_target = 100;
-static const int64_t vic_max_target = 1000;
 
 // mypacer.heart = heartbeat_init(vic_win_size, vic_buf_depth, vic_log_file, vic_min_target, vic_max_target);
 // mypacer.domid = getDomid();  
@@ -28,10 +23,10 @@ int main(int argc, char** argv)
 	printf("%d\n",mypacer.getSum() );
 
 
-	int domid=getDomid();//exec(R"(python3 -c 'from pyxs import Client;c=Client(xen_bus_path="/dev/xen/xenbus");c.connect();print((c.read("domid".encode())).decode());c.close()')"); 
-    printf("domid %d\n",domid);
-    mypacer.domid = domid;
-    printf("mypacer id %d\n",mypacer.domid );
+	// int domid=getDomid();//exec(R"(python3 -c 'from pyxs import Client;c=Client(xen_bus_path="/dev/xen/xenbus");c.connect();print((c.read("domid".encode())).decode());c.close()')"); 
+ //    printf("domid %d\n",domid);
+ //    mypacer.domid = domid;
+ //    printf("mypacer id %d\n",mypacer.domid );
 
 
 
