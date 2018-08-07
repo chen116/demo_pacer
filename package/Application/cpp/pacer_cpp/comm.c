8ecb732a24057fac82b370ba793cb2177cf86a86
Victor Chen <yvictorck@gmail.com>
2018-08-07 11:35:27 -0400

    a

--- package/Application/cpp/pacer_cpp/comm.c
+++ package/Application/cpp/pacer_cpp/comm.c
@@ -1,14 +1,8 @@
 
 #include "comm.h"
 #include <stdio.h>
-#include <heartbeats/heartbeat.h>
 #include <string.h>
-heartbeat_t* heart;
-static const int64_t vic_win_size = 10;
-static const int64_t vic_buf_depth = 1000;
-static const char* vic_log_file ="vic.log";
-static const int64_t vic_min_target = 100;
-static const int64_t vic_max_target = 1000;
+
 
 // #include <xenstore.h>
 #include <stdlib.h>
