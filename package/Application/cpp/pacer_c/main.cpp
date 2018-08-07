7dec2df2841194cecc7e326e0022f4c1f51193cc
Victor Chen <yvictorck@gmail.com>
2018-08-06 22:59:43 -0400

    a

--- package/Application/cpp/pacer_c/main.cpp
+++ package/Application/cpp/pacer_c/main.cpp
@@ -12,10 +12,10 @@ static const char* vic_log_file ="vic.log";
 static const int64_t vic_min_target = 100;
 static const int64_t vic_max_target = 1000;
 pacer mypacer;
-pacer.heart = heartbeat_init(vic_win_size, vic_buf_depth, vic_log_file, vic_min_target, vic_max_target);
-pacer.domid = getDomid();  
-pacer.xs = xs_daemon_open();
-pacer.base_path = xs_get_domain_path(xs, domid);
+mypacer.heart = heartbeat_init(vic_win_size, vic_buf_depth, vic_log_file, vic_min_target, vic_max_target);
+mypacer.domid = getDomid();  
+mypacer.xs = xs_daemon_open();
+mypacer.base_path = xs_get_domain_path(xs, domid);
 
 
 int main(int argc, char** argv)
@@ -27,7 +27,7 @@ int main(int argc, char** argv)
 
 
 
-    heartbeat_finish(pacer.heart);
+    heartbeat_finish(mypacer.heart);
 
 
     return 0;
